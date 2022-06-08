bfbd16d2ec8beed27de1d309cb6f4d69f418b4b9
Luiz Felipe <luiz.felipe.professional@hotmail.com>
2022-06-08 15:13:14 -0300

    Connecting to MongoDB

---
+++ MongoDBDemo/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBDemo;
+
+public class PersonModel
+{
+    [BsonId] // Identifier
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+}
+
